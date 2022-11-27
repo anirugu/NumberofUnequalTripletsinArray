3e2d5ebdfac0c2f3d2725383808af372e350ae81
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2022-11-27 16:37:55 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace NumberofUnequalTripletsinArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public int UnequalTriplets(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>();
+            for(int i = 0; i < nums.Length; i++)
+            {
+                HashSet<int> dist = new HashSet<int> { nums[i] };
+            }
+
+            HashSet<int> vals = nums.ToHashSet();
+            double div = nums.Length / 3;
+            if(vals.Count > (int)vals.Count)
+            {
+                return (int)div + 1;
+            }
+            else
+            {
+
+            }
+            if (vals.Count >= 3 && nums.Length == 3)
+                return 1;
+            if (vals.Count >= 3)
+                return vals.Count;
+            return 0;
+        }
+    }
+}
